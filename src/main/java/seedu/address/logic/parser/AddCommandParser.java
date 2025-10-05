39379c70aae8de65a2d9828e4153bc947d9cf4dd
Jiong Rui Tan <jruiballolol@gmail.com>
2025-10-05 14:46:16 +0800

    Update remark calling in and around code

--- src/main/java/seedu/address/logic/parser/AddCommandParser.java
+++ src/main/java/seedu/address/logic/parser/AddCommandParser.java
@@ -47,7 +47,7 @@ public class AddCommandParser implements Parser<AddCommand> {
         Remark remark = new Remark("");
         Set<Tag> tagList = ParserUtil.parseTags(argMultimap.getAllValues(PREFIX_TAG));
 
-        Person person = new Person(name, phone, email, address, tagList);
+        Person person = new Person(name, phone, email, address, remark, tagList);
 
         return new AddCommand(person);
     }
