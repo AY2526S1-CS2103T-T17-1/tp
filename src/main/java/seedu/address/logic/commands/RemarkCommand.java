b65f91bc869819960526e62eeed46019eeb34770
Jiong Rui Tan <jruiballolol@gmail.com>
2025-10-05 15:22:35 +0800

    Fix remark message format errors

--- src/main/java/seedu/address/logic/commands/RemarkCommand.java
+++ src/main/java/seedu/address/logic/commands/RemarkCommand.java
@@ -22,11 +22,12 @@ public class RemarkCommand extends Command {
     public static final String MESSAGE_ADD_REMARK_SUCCESS = "Added remark to Person: %1$s";
     public static final String MESSAGE_DELETE_REMARK_SUCCESS = "Removed remark from Person: %1$s";
 
-    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Edits the remark of the person identified "
-            + "by the index number used in the displayed person list. "
-            + "Parameters: INDEX " + PREFIX_REMARK + "REMARK\n"
-            + "Example: " + COMMAND_WORD + " 1 "
-            + PREFIX_REMARK + "Likes to swim.";
+public static final String MESSAGE_USAGE = COMMAND_WORD + ": Edits the remark of the person identified "
+        + "by the index number used in the displayed person list. "
+        + "Existing remark will be overwritten by the input.\n"
+        + "Parameters: INDEX (must be a positive integer) "
+        + PREFIX_REMARK + "REMARK\n"
+        + "Example: " + COMMAND_WORD + " 1 " + PREFIX_REMARK + "Likes to swim.";
 
     public static final String MESSAGE_NOT_IMPLEMENTED_YET = "Remark command not implemented yet";
     public static final String MESSAGE_ARGUMENTS = "Index: %1$d, Remark: %2$s";
@@ -89,8 +90,11 @@ public class RemarkCommand extends Command {
         return remark.value;
     }
 
-    private String generateSuccessMessage(Person personToEdit) {
-        String message = !remark.value.isEmpty() ? MESSAGE_ADD_REMARK_SUCCESS : MESSAGE_DELETE_REMARK_SUCCESS;
-        return String.format(message, Messages.format(personToEdit));
+    private String generateSuccessMessage(Person editedPerson) {
+        String message = !remark.value.isEmpty()
+                ? MESSAGE_ADD_REMARK_SUCCESS
+                : MESSAGE_DELETE_REMARK_SUCCESS;
+        return String.format(message, editedPerson);
     }
+
 }
